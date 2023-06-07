c12e5b5bdc9c42b41dc179e7991d1ed6053c7527
javiheev <116575814+Javihevia2003@users.noreply.github.com>
2023-06-07 20:09:20 +0200

    primerEjercicio

--- cultivos.js
+++ cultivos.js
@@ -14,5 +14,94 @@ console.log("hola");
 //     // si esta regado miramos cuanto lleva fecha1.diff(fecha2,"seconds")seconds
 //    } 
 // }, );
-"use strict"
-rl
+"use strict";
+
+const fs = require('fs');
+const { DateTime } = require("luxon");
+let cultivos = JSON.parse(fs.readFileSync('cultivosPlantar.json'));
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+/**
+ * Función que permite introducir por teclado
+ * @param {String} texto 
+ * @returns 
+ */
+function leeLinea(texto) {
+    return new Promise((resolve) => {
+        rl.question(texto, (introducido) => {
+            resolve(introducido);
+        });
+    });
+};
+
+// Función pora guardar los cambios en agenda.json
+function guardarCambios() {
+    fs.writeFileSync("cultivosPlantar.json", JSON.stringify(cultivos));
+}
+
+//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+// MENÚ
+//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+/**
+ * Función para mostrar el menú y leer la opción del usuario
+ * @returns {Array}
+ */
+async function mostrarMenu() {
+    console.log("\n***AGENDA***:\n");
+    console.log("1.Plantar cultivo");
+    console.log("2.Regar cultivo");
+    console.log("3.Recolectar cultivo");
+    console.log("4.Listar cultivo");
+    console.log("5.Eliminar cultivo");
+    console.log("Consultar cultivos recolectados")
+
+    let opcion = await leeLinea("Selecciona una opción:");
+    switch (opcion) {
+        case "1":
+            
+            break;
+        case "2":
+            
+            break;
+        case "3":
+           
+            break;
+        case "4":
+           
+            break;
+        case "5":
+            
+            break;
+        case "5":
+            
+            break;
+        default:
+            
+            break;
+    }
+};
+/******************************************************************************** */
+/*FUNCIONES */
+// 1. Plantar cultivo
+// Preguntará qué cultivo y plantará uno
+// Crea un cultivo plantado y dirá que se ha plantado y el identificador del cultivo (por
+// ejemplo: “patata_1”).
+async function plantarCultivo() {
+    cultivosDisponibles=[];
+    cultivosPlantados=[]
+    let pregunta=await leeLinea("¿Que tipo de cultivo quieres plantar?:")
+    for (const cultivo of cultivos) {
+        cultivosDisponibles.push(cultivo.nombre)
+        if(pregunta!=cultivo.nombre){
+            return 'No existe ese cultivo'
+        }
+
+    }
+}
