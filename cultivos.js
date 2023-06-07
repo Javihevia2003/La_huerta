56a90b1051e8b0f72948711ba9be3cfc5a96f40d
javiheev <116575814+Javihevia2003@users.noreply.github.com>
2023-06-07 19:40:55 +0200

    commit

---
+++ cultivos.js
@@ -0,0 +1,18 @@
+// diferenciar entre humedos y secos
+// 3.Mostrarátodos los que estan plantados
+// 5. se eliminan todos ncluidos los muertos
+// dos listas, plantado y recoolectado
+// la recoleccion solo corre cuando esta regando,
+// 4 tics por seundos dentro del intervalo;
+// fecha en la que se regó la planta
+// primera linea del intervalo: 
+console.log("hola");
+// setInterval(() => {
+//    const fecha=DateTime.local();
+//    for (const palntado of plantados) {
+//     // si esta muerto continue
+//     // si esta regado miramos cuanto lleva fecha1.diff(fecha2,"seconds")seconds
+//    } 
+// }, );
+"use strict"
+rl
